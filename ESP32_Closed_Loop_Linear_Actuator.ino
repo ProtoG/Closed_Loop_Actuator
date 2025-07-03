5dc10acd1488d6c3abb41b198847407040315968
ProtoG <33337919+ProtoG@users.noreply.github.com>
2025-07-03 16:34:17 -0400

    Add files via upload

---
+++ ESP32_Closed_Loop_Linear_Actuator.ino
@@ -0,0 +1,74 @@
+// Proto G Engineering
+//ESP32 Closed Loop Stepper Motor Test
+
+int directionPin = 22;
+int stepPin = 23;
+int numberOfStepsPerRevolution = 3200;
+int ledPin = 2;
+int pulseWidthMicros = 20;  // microseconds
+int millisbetweenSteps = 1; // milliseconds - or try 1000 for slower steps
+
+
+void setup() { 
+
+  Serial.begin(9600);
+  Serial.println("Starting StepperTest");
+  digitalWrite(ledPin, LOW);
+  
+  delay(2000);
+
+  pinMode(directionPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(2, OUTPUT);  
+ 
+  digitalWrite(directionPin, LOW);
+  for(int n = 0; n < (numberOfStepsPerRevolution*39); n++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(pulseWidthMicros); 
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(30);
+    digitalWrite(ledPin, !digitalRead(ledPin));
+  }
+  
+
+  delay(1000);
+  
+
+  digitalWrite(directionPin, HIGH);
+  for(int n = 0; n < (numberOfStepsPerRevolution*39); n++) {
+    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(15);    
+    digitalWrite(ledPin, !digitalRead(ledPin));
+  }
+/*
+  delay(1000);
+
+  digitalWrite(directionPin, HIGH);
+  for(int n = 0; n < (numberOfStepsPerRevolution*2); n++) {
+    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(10);    
+    digitalWrite(ledPin, !digitalRead(ledPin));
+  }
+
+  delay(1000);
+  
+
+  digitalWrite(directionPin, LOW);
+  for(int n = 0; n < (numberOfStepsPerRevolution*2); n++) {
+    digitalWrite(stepPin, HIGH);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(10);    
+    digitalWrite(ledPin, !digitalRead(ledPin));
+  }
+  */
+}
+ 
+
+
+
+
+
+void loop() { 
+}
